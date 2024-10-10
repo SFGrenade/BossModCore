5ab103d0f67b579ea772f27b810aa5d1c47261ad
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-10-10 20:32:29 +0200

    initial commit

---
+++ src/MonoBehaviours/CustomBossCopy.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace BossModCore.MonoBehaviours;
+
+[UsedImplicitly]
+public class CustomBossCopy : MonoBehaviour
+{
+    public static string copySceneName = "";
+
+}
